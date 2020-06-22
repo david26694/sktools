291946d8f62389ae039e21e1300454953d7288a0
david26694 <david26694@gmail.com>
2020-06-22 10:19:04 +0200

    Bootrstap cv improved

--- sktools/model_selection.py
+++ sktools/model_selection.py
@@ -17,6 +17,29 @@ class BootstrapFold:
         number of folds of our cross-validation setting
     size_fraction: float
         fraction of the training data being sampled. The lower, the bigger the test set
+    Example
+    -------
+    >>> import numpy as np
+    >>> from sktools.model_selection import BootstrapFold
+    >>> X = np.array([
+    >>>     np.random.randint(1, 3, 1000),
+    >>>     np.random.randint(0, 2, 1000)]
+    >>> ).T
+    >>> loo = BootstrapFold(10, size_fraction=1)
+    >>> for train_index, test_index in loo.split(X):
+    >>>     print(f"Train length: {len(train_index)} Test length: {len(test_index)}")
+    Train length: 1000 Test length: 393
+    Train length: 1000 Test length: 367
+    Train length: 1000 Test length: 372
+    Train length: 1000 Test length: 377
+    Train length: 1000 Test length: 361
+    Train length: 1000 Test length: 356
+    Train length: 1000 Test length: 366
+    Train length: 1000 Test length: 369
+    Train length: 1000 Test length: 390
+    Train length: 1000 Test length: 365
+
+
 
     References
     ----------
@@ -25,7 +48,7 @@ class BootstrapFold:
 
     """
 
-    def __init__(self, n_bootstraps, size_fraction):
+    def __init__(self, n_bootstraps=10, size_fraction=1):
 
         self.n_bootstraps = n_bootstraps
         self.size_fraction = size_fraction
@@ -41,7 +64,7 @@ class BootstrapFold:
         X = check_array(X)
 
         row_range = range(X.shape[0])
-        sample_size = round(self.size_fraction * len(row_range), 0)
+        sample_size = int(round(self.size_fraction * len(row_range), 0))
 
         for boot in range(self.n_bootstraps):
             train_idx = np.random.choice(row_range, sample_size)
